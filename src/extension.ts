321f2c22984e681e64eac4a5990f3af2e61e4bd6
Sehyun Chung <sehyun.chung@icloud.com>
2023-03-27 15:47:02 +0900

    Make users enable/disable from command pallete

--- src/extension.ts
+++ src/extension.ts
@@ -8,11 +8,38 @@ export function activate(context: vscode.ExtensionContext) {
         (editor.document.languageId === "typescript" ||
           editor.document.languageId === "typescriptreact")
       ) {
-        vscode.commands.executeCommand("typescript.restartTsServer");
+        restartTSServer();
       }
     })
   );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("tsserverRestarter.enable", enable)
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("tsserverRestarter.disable", disable)
+  );
 }
 
-// This method is called when your extension is deactivated
+function restartTSServer() {
+  const config = vscode.workspace.getConfiguration("tsserverRestarter");
+  const restartTSServerOnTabChange = config.get<boolean>("enabled");
+  if (!restartTSServerOnTabChange) {
+    return;
+  }
+  vscode.commands.executeCommand("typescript.restartTsServer");
+}
+
+async function enable() {
+  const config = vscode.workspace.getConfiguration("tsserverRestarter");
+  await config.update("enabled", true, vscode.ConfigurationTarget.Global);
+  vscode.window.showInformationMessage("TSServer Restarter: Enabled");
+}
+
+async function disable() {
+  const config = vscode.workspace.getConfiguration("tsserverRestarter");
+  await config.update("enabled", false, vscode.ConfigurationTarget.Global);
+  vscode.window.showInformationMessage("TSServer Restarter: Disabled");
+}
 export function deactivate() {}
